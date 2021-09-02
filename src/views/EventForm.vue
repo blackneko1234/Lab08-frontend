e7393fe2a6f4af4230749c53e23a24e63a38cc32
Decha_L <blackneko1234@hotmail.com>
2021-09-02 23:00:35 +0700

    8.6
    8.6

---
+++ src/views/EventForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+    <form>
+      <label>Category</label>
+      <input
+        v-model="event.category"
+        type="text"
+        placeholder="Category"
+        class="field"
+      />
+      <h3>Name & describe your event</h3>
+
+      <label>Title</label>
+      <input
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="field"
+      />
+
+      <label>Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
+
+      <h3>Where is your event?</h3>
+
+      <label>Location</label>
+      <input
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+      />
+      <button type="submit">Submit</button>
+    </form>
+    <pre>{{ event }}</pre>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            event:{
+                category:'',
+                title:'',
+                description:'',
+                location:''
+            }
+        }
+    }
+}
+</script>
